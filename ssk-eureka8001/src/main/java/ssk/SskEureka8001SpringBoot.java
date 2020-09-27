87918d3f8fb9440da0f19d8e409d82b64d93813a
sunsaike <sunsaike@163.com>
2020-09-27 17:23:21 +0800

    1、新增springboot启动类，新增yml文件，修改端口为8081 2、依赖sfl4j日志框架，实现为logback

--- ssk-eureka8001/src/main/java/ssk/SskEureka8001SpringBoot.java
+++ ssk-eureka8001/src/main/java/ssk/SskEureka8001SpringBoot.java
@@ -1,5 +1,12 @@
 package ssk;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@SpringBootApplication
 public class SskEureka8001SpringBoot {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplication().run(SskEureka8001SpringBoot.class,args);
+    }
 }
---
+++ ssk-eureka8001/src/main/java/ssk/controller/PaymentController.java
@@ -0,0 +1,16 @@
+package ssk.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class PaymentController {
+    @GetMapping("/paymentbyid/{id}")
+    public String getPaymentByid(@PathVariable("id") String id){
+        log.debug("@@@@@«Î«ÛgetPaymentByid£¨id="+id);
+        return "ID="+id+"SUCCCS";
+    }
+}
