761476c70bb4ac83032c5b2f6a0f03d0c5986e38
sunsaike <sunsaike@163.com>
2020-12-18 18:08:38 +0800

    444444

--- eureka-payment8001/src/main/java/ssk/SskEureka8001SpringBoot.java
+++ eureka-payment8001/src/main/java/ssk/SskEureka8001SpringBoot.java
@@ -1,13 +1,30 @@
 package ssk;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+import java.util.Arrays;
+
+@EnableAuthorizationServer
 @SpringBootApplication
 @EnableEurekaClient
 public class SskEureka8001SpringBoot {
     public static void main(String[] args) {
         SpringApplication.run(SskEureka8001SpringBoot.class,args);
+    }
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        return new ProviderManager(Arrays.asList(authenticationProvider));
     }
 }
